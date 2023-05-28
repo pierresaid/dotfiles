66b05b707f1f149218e103c002a6f5758cf520a9
pierresaid <said.pierre.emler@gmail.com>
2023-05-28 18:10:56 +0200

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,15 @@
+function la { Get-ChildItem -Force }
+function l { Get-ChildItem -Force }
+function ll { Get-ChildItem -Force }
+function q { Set-Location .. }
+function ll {
+    Clear-Host
+    Get-ChildItem -Force
+}
+function ne { notepad }
+function dd { Set-Location $HOME\Desktop\work }
+function ddd { Set-Location \\wsl.localhost\Ubuntu\home\saidp\work }
+function gs { git status }
+function ni { npm install }
+function nd { npm run dev }
+function nt { explorer . }
