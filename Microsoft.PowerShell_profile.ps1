4d7bba08f7e1f773bcab721cf1326885ea66f981
pierresaid <said.pierre.emler@gmail.com>
2025-08-25 22:07:56 +0200

    Enhance PowerShell profile with custom settings

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -13,3 +13,22 @@ function gs { git status }
 function ni { npm install }
 function nd { npm run dev }
 function nt { explorer . }
+
+$env:Path += ";C:\Users\Pierre Said\AppData\Local\Programs\oh-my-posh\bin"
+$env:POSH_THEMES_PATH = "C:\Users\Pierre Said\AppData\Local\Programs\oh-my-posh\themes"
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -Colors @{
+    "Command"   = [ConsoleColor]::Green
+    "Parameter" = [ConsoleColor]::Cyan
+    "String"    = [ConsoleColor]::Yellow
+    "Operator"  = [ConsoleColor]::Magenta
+    "Variable"  = [ConsoleColor]::White
+    "Number"    = [ConsoleColor]::DarkYellow
+    "Type"      = [ConsoleColor]::DarkCyan
+}
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
+Import-Module -Name Terminal-Icons
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\night-owl.omp.json" | Invoke-Expression
