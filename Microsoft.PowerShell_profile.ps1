db79638558d1b6ca49ac14878aef3fe4e3e2d240
pierresaid <said.pierre.emler@gmail.com>
2025-10-01 23:03:47 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,34 +1,31 @@
-function la { Get-ChildItem -Force }
-function l { Get-ChildItem -Force }
-function ll { Get-ChildItem -Force }
-function q { Set-Location .. }
-function ll {
-    Clear-Host
-    Get-ChildItem -Force
-}
-function ne { notepad }
+# Oh My Posh
+oh-my-posh init pwsh --config "$HOME\oh-my-posh\themes\night-owl.omp.json" | Invoke-Expression
+
+# PSReadLine
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+# Modules
+Import-Module Terminal-Icons
+
+# Aliases
+Set-Alias -Name vim -Value nvim
+Set-Alias -Name g -Value git
+
+# Useful functions
+function .. { Set-Location .. }
+function ... { Set-Location ..\.. }
+function ls { eza --icons }
+function ll {  Clear-Host eza -l --icons }
+function la { eza -la --icons }
 function dd { Set-Location $HOME\Desktop\work }
-function ddd { Set-Location \\wsl.localhost\Ubuntu\home\saidp\work }
+function ddd { Set-Location \\wsl.localhost\Ubuntu\home\azarias\work }
+function q { Set-Location .. }
+function c { Clear-Host }
 function gs { git status }
 function ni { npm install }
 function nd { npm run dev }
 function nt { explorer . }
-
-$env:Path += ";C:\Users\Pierre Said\AppData\Local\Programs\oh-my-posh\bin"
-$env:POSH_THEMES_PATH = "C:\Users\Pierre Said\AppData\Local\Programs\oh-my-posh\themes"
-
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
-Set-PSReadLineOption -Colors @{
-    "Command"   = [ConsoleColor]::Green
-    "Parameter" = [ConsoleColor]::Cyan
-    "String"    = [ConsoleColor]::Yellow
-    "Operator"  = [ConsoleColor]::Magenta
-    "Variable"  = [ConsoleColor]::White
-    "Number"    = [ConsoleColor]::DarkYellow
-    "Type"      = [ConsoleColor]::DarkCyan
-}
-Set-PSReadLineKeyHandler -Key Tab -Function Complete
-Import-Module -Name Terminal-Icons
-
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\night-owl.omp.json" | Invoke-Expression
